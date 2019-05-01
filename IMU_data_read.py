6407bb0c70ea1f74181dd9ec4882be679e20d243
Jack <514522970@qq.com>
2019-05-01 22:38:16 +0800

    20190501 finish reading the serial port data; finish analyse the data; but seems something wrong with the analysis; wait to see where is the question

--- IMU_data_read.py
+++ IMU_data_read.py
@@ -8,7 +8,8 @@ import threading
 Initialize_Num = 5
 Initialize_Threshold = 0.003
 Flag_Serial_Read = False
-
+Limb_Length = 200; #the length between the origin of the platform and the base
+Upper_Limb_Origin = np.array([[0],[0],[0],[1]])
 def Imu_Data_Decode_Init():
     '''
     wrap the imu_data_decode_init()
@@ -65,8 +66,13 @@ def Set_Initial_Pos():
 
     temp = np.delete(temp,-1,0)
     assert (temp.shape == (Initialize_Num,32))
+    average = np.average(temp, axis=0)
+    average = np.reshape(average,(1,32))
+    assert(average.shape == (1,32))
     print("Initialization Completed!")
-    return np.average(temp, axis=0)
+    print('The 1 IMU initial Quat is:',average[0][0:4])
+    print('The 2 IMU initial Quat is:',average[0][4:8])
+    return average
 
 def Serial_Read():
     while (Flag_Serial_Read):
@@ -77,6 +83,54 @@ def Serial_Read():
             Packet_Decode_w(buf[i])
 
         time.sleep(0.02)
+
+def Quat2R(q0,q1,q2,q3):
+    '''
+
+    :param q0:
+    :param q1:
+    :param q2:
+    :param q3:
+    :return:
+    '''
+    R = np.zeros([3,3])
+    R[0,0] = 1 - 2 * q2**2 - 2 * q3**2
+    R[0,1] = 2 * q1 * q2 - 2 * q0 * q3
+    R[0,2] = 2 * q1 * q3 + 2 * q0 * q2
+    R[1,0] = 2 * q1 * q2 + 2 * q0 * q3
+    R[1,1] = 1 - 2 * q1**2 - 2 * q3**2
+    R[1,2] = 2 * q2 * q3 - 2 * q0 * q1
+    R[2,0] = 2 * q1 * q3 - 2 * q0 * q2
+    R[2,1] = 2 * q2 * q3 + 2 * q0 * q1
+    R[2,2] = 1 - 2 * q1**2 - 2 * q2**2
+    assert (R.shape == (3,3))
+    return R
+
+def Cur_Quat2Relative_R(Relative_Zero_Points, Current_Points,Total_Arr_Length = 32):
+    assert (Current_Points.shape == (1,Total_Arr_Length))
+    assert (Relative_Zero_Points.shape == (1,Total_Arr_Length))
+    Rs02o = Quat2R(Relative_Zero_Points[0,0],Relative_Zero_Points[0,1],Relative_Zero_Points[0,2],Relative_Zero_Points[0,3])
+    Ru02o = Quat2R(Relative_Zero_Points[0,4],Relative_Zero_Points[0,5],Relative_Zero_Points[0,6],Relative_Zero_Points[0,7])
+    Rs2o = Quat2R(Current_Points[0,0],Current_Points[0,1],Current_Points[0,2],Current_Points[0,3])
+    Ru2o = Quat2R(Current_Points[0,4],Current_Points[0,5],Current_Points[0,6],Current_Points[0,7])
+    assert (Rs02o.shape == (3, 3))
+    assert (Ru02o.shape == (3, 3))
+    assert (Rs2o.shape == (3, 3))
+    assert (Ru2o.shape == (3, 3))
+    Ru2u0 = np.dot(np.linalg.inv(Ru02o),Ru2o)
+    Rs2s0 = np.dot(np.linalg.inv(Rs02o),Rs2o)
+    Ru2s = np.dot(np.linalg.inv(Rs2s0),Ru2u0)
+    assert (Ru2s.shape == (3,3))
+    return Ru2s
+
+def Get_Limb_Pos(Rot_Mat):
+    assert (Rot_Mat.shape == (3,3))
+    Trans = np.column_stack((Rot_Mat, np.zeros([3,1])))
+    Trans = np.row_stack((Trans, np.zeros((1,4))))
+    Linear_Trans = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,-Limb_Length],[0,0,0,1]])
+    Trans = np.dot(Trans,Linear_Trans)
+    u_o = np.dot(Trans, Upper_Limb_Origin)
+    return [u_o]
 
 if __name__ == '__main__':
     total_lib = cdll.LoadLibrary("./libtotal.so")
@@ -91,10 +145,15 @@ if __name__ == '__main__':
         ts = threading.Thread(target=Serial_Read, name='Serial_Read_Thread')
     except:
         raise EOFError
-    ts.start()
-    Quat_Relative_Zero_Point = Set_Initial_Pos()
-    print(Quat_Relative_Zero_Point[0:8])
-    Get_Quat(Quat)
+    ts.start() #start reading the data from the Serai port
+    Quat_Relative_Zero_Point = Set_Initial_Pos() #getting the initial position
+
+    while(1):
+        Get_Quat(Quat)
+        Rot_Mat_u2s = Cur_Quat2Relative_R(Quat_Relative_Zero_Point, (np.asarray(Quat)).reshape((1,-1)))
+        upper_o = Get_Limb_Pos(Rot_Mat_u2s)
+        print("The position of the upper limb:X:%0.3f\tY:%0.3f\tZ:%0.3f"%(upper_o[0][0,0],upper_o[0][1,0],upper_o[0][2,0]))
+
     Flag_Serial_Read = False
 
 
